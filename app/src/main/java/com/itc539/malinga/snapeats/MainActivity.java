06f42b031f80f2a8b8928a22fa7950ff0b95410a
Hasith Malinga <hasithmalinga89@gmail.com>
2018-09-18 00:12:29 +1000

    Initial Commit

---
+++ app/src/main/java/com/itc539/malinga/snapeats/MainActivity.java
@@ -0,0 +1,41 @@
+package com.itc539.malinga.snapeats;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnCapture;
+    ImageView imageView;
+    private final static int IMAGE_REQUEST_CODE = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnCapture = findViewById(R.id.btnCapture);
+        imageView = findViewById(R.id.imageView);
+
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, IMAGE_REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitMap = (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(bitMap);
+    }
+}
